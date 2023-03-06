46d53382a369ceb81f05d99eba026faa7d6285a2
DruidNoname <oboro.msu@gmail.com>
2023-03-06 22:26:14 +0300

    routing added except 404

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import Main from '../../pages/mainpage/mainpage';
+import Login from '../../pages/loginpage/loginpage';
+import Residence from '../../pages/residencepage/residencepage';
+import {AppRoutes} from '../../routes';
 
 type AppSettings = {
   amountOffers: number;
 }
 
 function App({amountOffers}: AppSettings): JSX.Element {
-  return <Main offers={amountOffers} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoutes.Root}
+          element={<Main offers={amountOffers} />}
+        />
+        <Route
+          path={AppRoutes.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoutes.Residence}
+          element={<Residence />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/routes.tsx
@@ -0,0 +1,5 @@
+export enum AppRoutes {
+  Root = '/',
+  Login = '/login',
+  Residence = '/offer/:id'
+}
