5457ef3cb6ef822e214407dc3be3e57639fd2957
DruidNoname <oboro.msu@gmail.com>
2023-04-20 14:14:04 +0300

    linter stuff

--- project/src/components/room-page/feedback-stuff/feedback-form/rating.tsx
+++ project/src/components/room-page/feedback-stuff/feedback-form/rating.tsx
@@ -1,7 +1,6 @@
 import {RATING_LEGEND} from '../../../../constants';
 import {RatingLegend} from '../../../../types/rating-legend';
 import React, {ChangeEventHandler, Fragment, MutableRefObject} from 'react';
-import {HandleFunction} from 'connect';
 
 type RatingProps = {
   ratingRef: MutableRefObject<HTMLInputElement[]>;
@@ -10,19 +9,19 @@ type RatingProps = {
 
 function Rating({ratingRef, onChangeHandler}: RatingProps) {
   return(
-  <div className="reviews__rating-form form__rating">
-    { RATING_LEGEND.map((star: RatingLegend, index: number): JSX.Element => (
-      <Fragment key={ star.title }>
-        <input ref={(el: HTMLInputElement) => (ratingRef.current[index] = el)} className="form__rating-input visually-hidden" name="rating" value={ star.rating.toString() } id={`${star.rating.toString()}-stars`} type="radio" onChange={ onChangeHandler }/>
-        <label htmlFor={`${star.rating.toString()}-stars`} className="reviews__rating-label form__rating-label" title={ star.title }>
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-      </Fragment>
-    ))}
-  </div>
-  )
+    <div className="reviews__rating-form form__rating">
+      { RATING_LEGEND.map((star: RatingLegend, index: number): JSX.Element => (
+        <Fragment key={ star.title }>
+          <input ref={(el: HTMLInputElement) => (ratingRef.current[index] = el)} className="form__rating-input visually-hidden" name="rating" value={ star.rating.toString() } id={`${star.rating.toString()}-stars`} type="radio" onChange={ onChangeHandler }/>
+          <label htmlFor={`${star.rating.toString()}-stars`} className="reviews__rating-label form__rating-label" title={ star.title }>
+            <svg className="form__star-image" width="37" height="33">
+              <use xlinkHref="#icon-star"></use>
+            </svg>
+          </label>
+        </Fragment>
+      ))}
+    </div>
+  );
 }
 
 export default Rating;
